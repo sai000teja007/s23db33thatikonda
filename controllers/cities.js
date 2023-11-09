5a45951ecf909da0eaf7fc78c5ccc305d50befdb
sai000teja007 <137807543+sai000teja007@users.noreply.github.com>
2023-11-08 23:33:02 -0600

    added ss8

--- controllers/cities.js
+++ controllers/cities.js
@@ -45,3 +45,23 @@ exports.cities_view_all_Page = async function(req, res) {
     }
 };
     
+// Handle Costume create on POST.
+exports.cities_create_post = async function(req, res) {
+console.log(req.body)
+let document = new cities();
+// We are looking for a body, since POST does not have query parameters.
+// Even though bodies can be in many different formats, we will be picky
+// and require that it be a json object
+// {"costume_type":"goat", "cost":12, "size":"large"}
+document.cityname = req.body.cityname;
+document.citypopulation = req.body.citypopulation;
+document.cityrevenue = req.body.cityrevenue;
+try{
+let result = await document.save();
+res.send(result);
+}
+catch(err){
+res.status(500);
+res.send(`{"error": ${err}}`);
+}
+};
