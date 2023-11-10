e67e62454ec17949f6f9644977ae7ed28994b0c2
sai000teja007 <137807543+sai000teja007@users.noreply.github.com>
2023-11-10 11:46:18 -0600

    added ss8

--- controllers/cities.js
+++ controllers/cities.js
@@ -1,21 +1,21 @@
 var cities = require('../models/cities');
-// List of all Costumes
+// List of all Cities
 exports.cities_list = function(req, res) {
 res.send('NOT IMPLEMENTED: cities list');
 };
-// for a specific Costume.
+// for a specific Cities.
 exports.cities_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: cities detail: ' + req.params.id);
 };
-// Handle Costume create on POST.
+// Handle Cities create on POST.
 exports.cities_create_post = function(req, res) {
 res.send('NOT IMPLEMENTED: cities create POST');
 };
-// Handle Costume delete form on DELETE.
+// Handle City delete form on DELETE.
 exports.cities_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: cities delete DELETE ' + req.params.id);
 };
-// Handle Costume update form on PUT.
+// Handle Citiese update form on PUT.
 exports.cities_update_put = function(req, res) {
 res.send('NOT IMPLEMENTED: cities update PUT' + req.params.id);
 };
@@ -45,7 +45,7 @@ exports.cities_view_all_Page = async function(req, res) {
     }
 };
     
-// Handle Costume create on POST.
+// Handle Cities create on POST.
 exports.cities_create_post = async function(req, res) {
 console.log(req.body)
 let document = new cities();
@@ -64,4 +64,37 @@ catch(err){
 res.status(500);
 res.send(`{"error": ${err}}`);
 }
+};
+
+exports.cities_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await cities.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+       // res.send({"error": document for id ${req.params.id} not found);
+    }
+};
+
+//New code for Assignment 12 for screenshot2
+//Handle cities update form on PUT
+exports.cities_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await cities.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.cityname)
+            toUpdate.cityname = req.body.cityname;
+        if (req.body.citypopulation) toUpdate.citypopulation= req.body.citypopulation;
+        if (req.body.cityrevenue) toUpdate.cityrevenue= req.body.cityrevenue;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+    }
 };
