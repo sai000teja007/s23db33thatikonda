0bd367bde2f867eeac0983e838cad7c9fcd676b1
sai000teja007 <137807543+sai000teja007@users.noreply.github.com>
2023-11-15 21:03:30 -0600

    Added everthing

--- controllers/cities.js
+++ controllers/cities.js
@@ -120,3 +120,43 @@ res.status(500)
 res.send(`{'error': '${err}'}`);
 }
 };
+// Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.cities_create_Page = function(req, res) {
+console.log("create view")
+try{
+res.render('citiescreate', { title: 'cities Create'});
+}
+catch(err){
+res.status(500)
+res.send(`{'error': '${err}'}`);
+}
+};
+// Handle building the view for updating a costume.
+// query provides the id
+exports.cities_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await cities.findById(req.query.id)
+    res.render('citiesupdate', { title: 'Cities Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+// Handle a delete one view with id from query
+exports.cities_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await cities.findById(req.query.id)
+    res.render('citiesdelete', { title: 'cities Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
--- routes/cities.js
+++ routes/cities.js
@@ -6,3 +6,9 @@ router.get('/', cities_controlers.cities_view_all_Page );
 module.exports = router;
 /* GET detail cities page */
 router.get('/detail', cities_controlers.cities_view_one_Page);
+/* GET create costume page */
+router.get('/create', cities_controlers.cities_create_Page);
+/* GET create update page */
+router.get('/update', cities_controlers.cities_update_Page);
+/* GET delete costume page */
+router.get('/delete', cities_controlers.cities_delete_Page);
