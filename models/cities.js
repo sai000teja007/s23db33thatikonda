a7628547db996d5b1a830fffac4352c27a834dc8
sai000teja007 <137807543+sai000teja007@users.noreply.github.com>
2023-11-29 17:04:19 -0600

    created a login page, added validator and added button for each collection

--- models/cities.js
+++ models/cities.js
@@ -1,7 +1,18 @@
 const mongoose = require("mongoose")
 const citySchema = mongoose.Schema({
-cityname: String,
-citypopulation: Number,
-cityrevenue: Number
+    cityname: {
+        type: String,
+        minlength: 2,
+        maxlength: 20,
+    },
+
+    citypopulation: {
+        type: Number,
+    },
+    cityrevenue: {
+        type:Number,
+        min: 0,
+        max: 5000,
+    }
 })
 module.exports = mongoose.model("cities", citySchema)
